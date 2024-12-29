6816be2476ba43de46c57e5dcc0ee4bbb1336a19
Siddheshya <siddheshya007@gmail.com>
2024-12-29 22:33:01 +0530

    added review class

--- src/main/java/com/example/UberReviewService/Models/Review.java
+++ src/main/java/com/example/UberReviewService/Models/Review.java
@@ -1,15 +1,34 @@
 package com.example.UberReviewService.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.annotation.processing.Generated;
+import java.util.Date;
 
-@Entity
+@Entity // this annotation deals with java logic layer
+@Table(name = "bookingReview") // this annotation deals with database layer
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
+    @Column(nullable = false)
+    String content;
+
+
+    double rating;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate // only handle it for object creation
+    @Column(nullable = false)
+    Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP) // this annotation will tell the format in which we want to store
+    @Column(nullable = false)
+    @LastModifiedDate   // only handle it when object modified
+    Date updatedAt;
 
 }
